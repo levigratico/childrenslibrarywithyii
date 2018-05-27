4e4f22dd34cabfb77d87c8845c267821c67c0f8c
levigratico <levigratico@gmail.com>
2018-05-27 20:05:11 +0800

    add zoom function

--- web/js/bookdesc.js
+++ web/js/bookdesc.js
@@ -8,36 +8,35 @@ $(document).ready(function(){
 
 	});
 
-	var slider = $("#main-slider").lightSlider({
-				    gallery:true,
-				    item:1,
-				    thumbItem:9,
-				    slideMargin: 0,
-				    loop:true,
-				    onSliderLoad: function() {
-	                    $('#main-slider').removeClass('cS-hidden');
-	                }  
-				});
+	var overlay = document.getElementById("overlay");
+	var viewer = new Viewer(overlay, {
+        url: 'data-original',
+        title: false,
+        toolbar: false,
+        fullscreen: true
+      });
+
+	viewer.show();
+
+	var viewerContainer = document.getElementsByClassName("viewer-container")[0];
+	viewerContainer = $(viewerContainer);
+	viewerContainer.append('<div class="vSAction"><a class="vSPrev"></a><a class="vSNext"></a></div>');
+	viewerContainer.on('click', '.vSPrev', function(){
+		viewer.prev(true);
+	});
 
 
-	$("#overlay").click(function(e){
-		if (e.target !== this)
-	    return;
-		$("#overlay").hide();
-		return false;
+	viewerContainer.on('click', '.vSNext', function(){
+		viewer.next(true);
 	});
 
 	$(".thumbnail").click(function(el){
 		var index = $(this).data("id");
-		$("#overlay").show();  
-		slider.goToSlide(index);
+		viewer.view(index);
 	}); 
 
 	$(document).on('click', '#readmore', function() {
-		$("#overlay").show();  
+		viewer.show();  
 	});
-
-	
-
 });
 
